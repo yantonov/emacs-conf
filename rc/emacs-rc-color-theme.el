06b5b51ea22a9916e239371a01d93f59b3a4eadb
Yury Antonov <yantonov@yandex.ru>
2012-04-16 22:54:35 +0600

    Added gentooish color theme. Set as default.

--- rc/emacs-rc-color-theme.el
+++ rc/emacs-rc-color-theme.el
@@ -1,11 +1,13 @@
 ;; color-theme
 ;; additional-themes http://jblevins.org/projects/emacs-color-themes/
+;; enable color theme mode
 (add-to-list 'load-path (concat emacs-mode-home "color-theme-6.6.0"))
+;; custom color themes
 (add-to-list 'load-path (concat emacs-home "color-themes"))
 
 (require 'color-theme)
-(require 'color-theme-subdued)
+(require 'gentooish)
 (eval-after-load "color-theme"
   '(progn
      (color-theme-initialize)
-     (color-theme-subdued)))
+     (color-theme-gentooish)))
