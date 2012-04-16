827505346413bbd95b30e6bbebfbba1b94f98f6d
Yury Antonov <yantonov@yandex.ru>
2012-04-16 23:38:32 +0600

    Location for linum mode is explicitly defined in init.el

--- init.el
+++ init.el
@@ -11,6 +11,8 @@
 (add-to-list 'load-path (concat emacs-mode-home "color-theme-6.6.0"))
 ;; custom color themes
 (add-to-list 'load-path (concat emacs-home "color-themes"))
+;; linum
+(add-to-list 'load-path (concat emacs-mode-home "linumplus/"))
 
 ;; general editor settings
 (load "~/emacs/rc/emacs-rc-editor.el")
--- rc/emacs-rc-linum.el
+++ rc/emacs-rc-linum.el
@@ -1,5 +1,4 @@
 ;; linum (line numbers) settings
-(add-to-list 'load-path (concat emacs-mode-home "linumplus/"))
 (require 'linum+)
 (setq linum-format "%d ")
 (global-linum-mode 1)
