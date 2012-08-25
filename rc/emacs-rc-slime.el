7f5967448b2fe726ecffec7aee561776bff834a8
Yury Antonov <yantonov@yandex.ru>
2012-08-25 23:29:03 +0600

    Removed unused slime settings.

--- rc/emacs-rc-slime.el
+++ rc/emacs-rc-slime.el
@@ -1,16 +1,5 @@
-(eval-after-load "slime" 
-  '(progn (slime-setup '(slime-repl))	
-	(defun paredit-mode-enable () (paredit-mode 1))	
-	(add-hook 'slime-mode-hook 'paredit-mode-enable)	
-	(add-hook 'slime-repl-mode-hook 'paredit-mode-enable)
-	
-	;; encoding
-	(setq slime-net-coding-system 'utf-8-unix)
+;; encoding
+(setq slime-net-coding-system 'utf-8-unix)
 
-	;; ignore version's disperancy
-	(setq slime-protocol-version 'ignore)
-	))
-(require 'slime)
-(slime-setup)
-(setq slime-use-autodoc-mode nil)
-(global-set-key [f5] 'slime)
+;; ignore version's disperancy
+(setq slime-protocol-version 'ignore)
