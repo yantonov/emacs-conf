6bbe55d861b3c838ec862b33b2aa10881ef7d6f1
Yury Antonov <yantonov@yandex.ru>
2012-09-04 00:13:15 +0600

    Added nrepl support for emacs 23.

--- init.el
+++ init.el
@@ -1,27 +1,47 @@
-; elpa settings
-(load "~/emacs/rc/emacs-rc-elpa.el")
 ;; paths, environment
 (load "~/emacs/rc/emacs-rc-env.el")
 
+;; 
 ;; used modes
+;;
+
+;; package manager
+;; http://repo.or.cz/w/emacs.git/blob_plain/1a0a666f941c99882093d7bd08ced15033bc3f0c:/lisp/emacs-lisp/package.el
+(add-to-list 'load-path (concat emacs-mode-home "package"))
+
 ;; highlight parentheses
 ;; http://www.emacswiki.org/emacs/HighlightParentheses
 (add-to-list 'load-path (concat emacs-mode-home "highlight-parentheses/"))
 (require 'highlight-parentheses)
+
 ;; color theme mode
 (add-to-list 'load-path (concat emacs-mode-home "color-theme-6.6.0"))
+
 ;; custom color themes
 (add-to-list 'load-path (concat emacs-home "color-themes"))
+
 ;; linum
 ;; http://www.emacswiki.org/emacs/LinumPlus
 ;; trunk
 ;; http://code.google.com/p/dea/source/browse/trunk/my-lisps/linum%2B.el
 ;; download to mode/linumplus/linum+.el
 (add-to-list 'load-path (concat emacs-mode-home "linumplus/"))
+
 ;; paredit
 ;; http://mumble.net/~campbell/emacs/paredit.el
 ;; download to mode/paredit/paredit.el
 (add-to-list 'load-path (concat emacs-mode-home "paredit/"))
+
+;; nrepl 
+;; https://github.com/kingtim/nrepl.el
+(add-to-list 'load-path (concat emacs-mode-home "nrepl/"))
+
+;; 
+;; settings
+;;
+
+; elpa settings
+(load "~/emacs/rc/emacs-rc-elpa.el")
 
 ;; general editor settings
 (load "~/emacs/rc/emacs-rc-editor.el")
