4238fb22e313700c672bc7f6ad00df0d317f5294
Yury Antonov <yantonov@yandex.ru>
2012-10-01 23:12:08 +0600

    Added install script for linum.

---
+++ install/install-linumplus.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# linum
+# http://www.emacswiki.org/emacs/LinumPlus
+# trunk
+# http://code.google.com/p/dea/source/browse/trunk/my-lisps/linum%2B.el
+
+# init env
+
+CURRENT_DIR=`dirname $0`
+UTIL_DIR="${CURRENT_DIR}/shell-util"
+WD=`pwd`
+cd $UTIL_DIR
+UTIL_DIR=`pwd`
+cd $WD
+EMACS_HOME=`${UTIL_DIR}/emacs-home.sh`
+EMACS_MODE_HOME=`${UTIL_DIR}/emacs-mode-home.sh`
+
+# run
+
+INSTALLED_MODE_EL_FILE_URL="http://code.google.com/p/dea/source/browse/trunk/my-lisps/linum%2B.el"
+INSTALLED_MODE_DIR="linumplus"
+
+cd ${EMACS_MODE_HOME}
+`${UTIL_DIR}/mkdir.sh ${INSTALLED_MODE_DIR}`
+cd ${INSTALLED_MODE_DIR}
+
+wget ${INSTALLED_MODE_EL_FILE_URL}
+
+
