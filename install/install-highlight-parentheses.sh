42d653db7d728453fe7738f7680eb52c53f606a7
Yury Antonov <yantonov@yandex.ru>
2012-11-12 05:28:18 +0600

    White space fix for highlight parentheses.

--- install/install-highlight-parentheses.sh
+++ install/install-highlight-parentheses.sh
@@ -5,10 +5,5 @@
 # direct url
 # http://nschum.de/src/emacs/highlight-parentheses/highlight-parentheses.el
 
-# init env
-
 SCRIPT_DIR=`dirname $0`
 $SCRIPT_DIR/util-install-single-file.sh "http://nschum.de/src/emacs/highlight-parentheses/highlight-parentheses.el" "highlight-parentheses"
-
-
-
