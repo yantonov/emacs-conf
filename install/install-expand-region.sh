eaaed858e1e4eb7274239c966efd1a3f11e35576
Yury Antonov <yantonov@yandex.ru>
2012-11-12 05:17:25 +0600

    Renamed install script for git repo. Install script for expand-region was rewrited using util script.

--- install/install-expand-region.sh
+++ install/install-expand-region.sh
@@ -1,31 +1,7 @@
 #!/bin/bash
 
-# page
-# https://github.com/magnars/expand-region.el.git
-
-# init env
-
-CURRENT_DIR=`dirname $0`
-UTIL_DIR="${CURRENT_DIR}/shell-util"
-WD=`pwd`
-cd $UTIL_DIR
-UTIL_DIR=`pwd`
-cd $WD
-EMACS_HOME=`${UTIL_DIR}/emacs-home.sh`
-EMACS_MODE_HOME=`${UTIL_DIR}/emacs-mode-home.sh`
-
-# run
-
-GIT_REPO="https://github.com/magnars/expand-region.el.git"
-INSTALLED_MODE_DIR="expand-region"
-
-cd ${EMACS_MODE_HOME}
-rm -rf ${INSTALLED_MODE_DIR}
-`${UTIL_DIR}/mkdir.sh ${INSTALLED_MODE_DIR}`
-cd ${INSTALLED_MODE_DIR}
-
-git clone ${GIT_REPO} .
-
+SCRIPT_DIR=`dirname $0`
+$SCRIPT_DIR/util-install-from-git.sh "https://github.com/magnars/expand-region.el.git" "expand-region"
 
 
 
--- install/install-string-manipulation.sh
+++ install/install-string-manipulation.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SCRIPT_DIR=`dirname $0`
-$SCRIPT_DIR/util-install-using-git.sh "https://github.com/magnars/s.el.git" "string-manipulation"
+$SCRIPT_DIR/util-install-from-git.sh "https://github.com/magnars/s.el.git" "string-manipulation"
