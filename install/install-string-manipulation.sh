9ad6af279f276b83ef9bb03ad6b2069355591c20
Yury Antonov <yantonov@yandex.ru>
2012-11-12 05:13:24 +0600

    Added general script to install modes from git repositories.

--- install/install-string-manipulation.sh
+++ install/install-string-manipulation.sh
@@ -1,27 +1,4 @@
 #!/bin/bash
 
-# page
-# https://github.com/magnars/s.el
-
-# init env
-
-CURRENT_DIR=`dirname $0`
-UTIL_DIR="${CURRENT_DIR}/shell-util"
-WD=`pwd`
-cd $UTIL_DIR
-UTIL_DIR=`pwd`
-cd $WD
-EMACS_HOME=`${UTIL_DIR}/emacs-home.sh`
-EMACS_MODE_HOME=`${UTIL_DIR}/emacs-mode-home.sh`
-
-# run
-
-GIT_REPO="https://github.com/magnars/s.el.git"
-INSTALLED_MODE_DIR="string-manipulation"
-
-cd ${EMACS_MODE_HOME}
-rm -rf ${INSTALLED_MODE_DIR}
-`${UTIL_DIR}/mkdir.sh ${INSTALLED_MODE_DIR}`
-cd ${INSTALLED_MODE_DIR}
-
-git clone ${GIT_REPO} .
+SCRIPT_DIR=`dirname $0`
+$SCRIPT_DIR/util-install-using-git.sh "https://github.com/magnars/s.el.git" "string-manipulation"
---
+++ install/util-install-using-git.sh
@@ -0,0 +1,35 @@
+#/bin/bash
+
+# script install emacs mode (util, library) from given git repo
+
+# Usage: 
+# first argument - git repo url
+# second argument - dir name for library at emacs/mode
+
+if [ $# -lt 2 ]; then
+    echo "Usage: scriptname git_repo mod_dir_name"
+    exit 0
+else
+    GIT_REPO=$1
+    INSTALLED_MODE_DIR=$2
+fi
+    
+# init env
+
+CURRENT_DIR=`dirname $0`
+UTIL_DIR="${CURRENT_DIR}/shell-util"
+WD=`pwd`
+cd $UTIL_DIR
+UTIL_DIR=`pwd`
+cd $WD
+EMACS_HOME=`${UTIL_DIR}/emacs-home.sh`
+EMACS_MODE_HOME=`${UTIL_DIR}/emacs-mode-home.sh`
+
+# run
+
+cd ${EMACS_MODE_HOME}
+rm -rf ${INSTALLED_MODE_DIR}
+`${UTIL_DIR}/mkdir.sh ${INSTALLED_MODE_DIR}`
+cd ${INSTALLED_MODE_DIR}
+
+git clone ${GIT_REPO} .
