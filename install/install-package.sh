7adbf4182dac99b9f5f81d9c5ec638c83e7afcfe
Yury Antonov <yantonov@yandex.ru>
2012-11-12 05:31:18 +0600

    Install script for package was rewrited using util script.

--- install/install-package.sh
+++ install/install-package.sh
@@ -1,26 +1,5 @@
 #/bin/bash
 
-# init env
-
-CURRENT_DIR=`dirname $0`
-UTIL_DIR="${CURRENT_DIR}/shell-util"
-WD=`pwd`
-cd $UTIL_DIR
-UTIL_DIR=`pwd`
-cd $WD
-EMACS_HOME=`${UTIL_DIR}/emacs-home.sh`
-EMACS_MODE_HOME=`${UTIL_DIR}/emacs-mode-home.sh`
-
-# run
-
-PACKAGE_EL_URL="http://repo.or.cz/w/emacs.git/blob_plain/1a0a666f941c99882093d7bd08ced15033bc3f0c:/lisp/emacs-lisp/package.el"
-INSTALLED_MODE_DIR="package"
-
-cd ${EMACS_MODE_HOME}
-`${UTIL_DIR}/mkdir.sh ${INSTALLED_MODE_DIR}`
-cd ${INSTALLED_MODE_DIR}
-
-rm -rf *
-wget ${INSTALLED_MODE_EL_FILE_URL}
-
+SCRIPT_DIR=`dirname $0`
+$SCRIPT_DIR/util-install-single-file.sh "http://repo.or.cz/w/emacs.git/blob_plain/1a0a666f941c99882093d7bd08ced15033bc3f0c:/lisp/emacs-lisp/package.el" "package"
 
