bcff8bb2163099c461926cf47a48df50905a192f
Yury Antonov <yantonov@yandex.ru>
2012-11-12 05:19:50 +0600

    install script for zencoding was rewrited using util script.

--- install/install-zencoding.sh
+++ install/install-zencoding.sh
@@ -1,31 +1,7 @@
 #!/bin/bash
 
-# page
-# https://github.com/rooney/zencoding.git
-
-# init env
-
-CURRENT_DIR=`dirname $0`
-UTIL_DIR="${CURRENT_DIR}/shell-util"
-WD=`pwd`
-cd $UTIL_DIR
-UTIL_DIR=`pwd`
-cd $WD
-EMACS_HOME=`${UTIL_DIR}/emacs-home.sh`
-EMACS_MODE_HOME=`${UTIL_DIR}/emacs-mode-home.sh`
-
-# run
-
-GIT_REPO="https://github.com/rooney/zencoding.git"
-INSTALLED_MODE_DIR="zencoding-mode"
-
-cd ${EMACS_MODE_HOME}
-rm -rf ${INSTALLED_MODE_DIR}
-`${UTIL_DIR}/mkdir.sh ${INSTALLED_MODE_DIR}`
-cd ${INSTALLED_MODE_DIR}
-
-git clone ${GIT_REPO} .
-
+SCRIPT_DIR=`dirname $0`
+$SCRIPT_DIR/util-install-from-git.sh "https://github.com/rooney/zencoding.git" "zencoding-mode"
 
 
 
