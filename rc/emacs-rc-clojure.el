e0691353d90e839de049afcd2510265ed4d4c205
Yury Antonov <yantonov@yandex.ru>
2012-12-04 06:45:34 +0600

    Added requirements for clojure mode (more precisely for hook).

--- rc/emacs-rc-clojure.el
+++ rc/emacs-rc-clojure.el
@@ -1,8 +1,9 @@
-;; *** clojure-mode BEGIN
+(require 'paredit)
+(require 'highlight-parentheses)
+
 (defun my-clojure-mode-hook ()
   (paredit-mode 1)
   (local-set-key [return] 'newline-and-indent)
   (highlight-parentheses-mode t)
   )
 (add-hook 'clojure-mode-hook 'my-clojure-mode-hook)
-;; *** clojure-mode END
