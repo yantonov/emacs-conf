859219d3baa26e7c0416ce3b26e50eb3a51aa7db
Yury Antonov <yantonov@yandex.ru>
2012-12-23 04:41:01 +0600

    Improved install script for clojure-mode, popup (added init code for elpa).

--- install/util/install-popup.el
+++ install/util/install-popup.el
@@ -1,2 +1,7 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
 (when (not (package-installed-p 'popup))
   (package-install 'popup))
