5e021db32a901f4b86ec1a0a2b50e90d590d3c0c
Yury Antonov <yantonov@yandex.ru>
2012-12-22 15:35:51 +0600

    Full review of nrepl config.

--- rc/emacs-rc-nrepl.el
+++ rc/emacs-rc-nrepl.el
@@ -7,12 +7,6 @@
 ; Stop the error buffer from popping up while working in the REPL buffer:
 (setq nrepl-popup-stacktraces nil)
 
-; only indent for tab
-;(setq nrepl-tab-command 'indent-for-tab-command)
-
-(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
-(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
-
 (eval-after-load "auto-complete"
    '(add-to-list 'ac-modes 'nrepl-mode))
 
@@ -23,16 +17,17 @@
 (defun my-nrepl-mode-hook ()
   (paredit-mode 1)
   (highlight-parentheses-mode t)
+  
+  (ac-nrepl-setup)
   (set-auto-complete-as-completion-at-point-function)
   )
 
 (defun my-nrepl-interaction-mode-hook ()
-  (paredit-mode 1)
-  (highlight-parentheses-mode t)
+  (ac-nrepl-setup)
   (set-auto-complete-as-completion-at-point-function)
-  ; using ac-nrepl's popup documentation in place of nrepl-doc
   (define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
   )
 
 (add-hook 'nrepl-mode-hook 'my-nrepl-mode-hook)
-(add-hook 'nrepl-interaction-mode-hook 'my-nrepl-interactive-mode-hook)
+(add-hook 'nrepl-interaction-mode-hook 'my-nrepl-interaction-mode-hook)
+
