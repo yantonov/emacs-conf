24659c7eaa27a18e861aeebb3274ef726ab5d1c2
Yury Antonov <yantonov@yandex.ru>
2012-12-23 05:17:00 +0600

    Another global hotkeys movement.

--- rc/emacs-rc-editor.el
+++ rc/emacs-rc-editor.el
@@ -1,20 +1,11 @@
 ;; [ general editor settings ]
-;; newline and indent
-(global-set-key "\r" 'newline-and-indent)
 ;; set goal colulm
 ;; http://emacsblog.org/2007/03/17/quick-tip-set-goal-column/
 (put 'set-goal-column 'disabled nil)
-;; windows movement
-;; http://www.emacswiki.org/emacs/WindMove
-(when (fboundp 'windmove-default-keybindings)
-      (windmove-default-keybindings))
-;; too long lines shows/hides by "C-C l"
-(global-set-key (kbd "C-c l") 'toggle-truncate-lines)
 ;; Show column-number in the mode line
 (column-number-mode 1)
 ;; delete selected region on backspace, del, ctrl+d
 (delete-selection-mode t)
-
 ;;; [ ui ]
 ;; hide splash screen
 (setq inhibit-splash-screen t)
--- rc/emacs-rc-global-hotkeys.el
+++ rc/emacs-rc-global-hotkeys.el
@@ -7,3 +7,12 @@
 (global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
 (global-set-key (kbd "S-C-<down>") 'shrink-window)
 (global-set-key (kbd "S-C-<up>") 'enlarge-window)
+
+;; windows movement
+;; http://www.emacswiki.org/emacs/WindMove
+(when (fboundp 'windmove-default-keybindings)
+      (windmove-default-keybindings))
+;; too long lines shows/hides by "C-C l"
+(global-set-key (kbd "C-c l") 'toggle-truncate-lines)
+;; newline and indent
+(global-set-key "\r" 'newline-and-indent)
