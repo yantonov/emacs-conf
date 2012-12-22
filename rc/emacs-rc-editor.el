6d041d51006a32d6e281ce09709d8ce93238d2cf
Yury Antonov <yantonov@yandex.ru>
2012-12-22 12:12:15 +0600

    Global hotkeys moved corresponding config file.

--- rc/emacs-rc-editor.el
+++ rc/emacs-rc-editor.el
@@ -24,9 +24,4 @@
 ; tabls
 (setq tab-width 4)
 (setq indent-tabs-mode nil)
-; resize buffer key bindings
-; http://www.emacswiki.org/emacs/WindowResize
-(global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
-(global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
-(global-set-key (kbd "S-C-<down>") 'shrink-window)
-(global-set-key (kbd "S-C-<up>") 'enlarge-window)
+
--- rc/emacs-rc-global-hotkeys.el
+++ rc/emacs-rc-global-hotkeys.el
@@ -1,1 +1,9 @@
-(global-set-key (kbd "C-M-l") 'indent-buffer) ; inspired by intellij idea
+; inspired by intellij idea
+(global-set-key (kbd "C-M-l") 'indent-buffer) 
+
+; resize buffer key bindings
+; http://www.emacswiki.org/emacs/WindowResize
+(global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
+(global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "S-C-<down>") 'shrink-window)
+(global-set-key (kbd "S-C-<up>") 'enlarge-window)
