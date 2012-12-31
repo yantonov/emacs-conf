1d54344dd8bed59b51b3718b51e83d1466996789
Yury Antonov <yantonov@yandex.ru>
2012-12-31 21:25:55 +0600

    Added function to kill buffer and delete corresponding file.

--- rc/emacs-rc-global-hotkeys.el
+++ rc/emacs-rc-global-hotkeys.el
@@ -23,4 +23,6 @@
 ;; defined in rc/emacs-rc-editor
 (global-set-key (kbd "<C-return>") 'open-line-below)
 (global-set-key (kbd "<C-S-return>") 'open-line-above)
+
 (global-set-key (kbd "C-x C-r") 'rename-current-buffer-file)
+(global-set-key (kbd "C-x C-k") 'delete-current-buffer-file)
