6d398ba5efdecd4898e84fd0d95d8a4491f30e68
Yury Antonov <yantonov@yandex.ru>
2013-02-07 01:10:36 +0600

    Solarized theme is selected by default.

--- rc/emacs-rc-color-theme.el
+++ rc/emacs-rc-color-theme.el
@@ -2,10 +2,9 @@
 ;; additional-themes http://jblevins.org/projects/emacs-color-themes/
 (require 'color-theme)
 
-(require 'bubbleberry-theme)
-;; (require 'color-theme-solarized)
-;; (eval-after-load "color-theme"
-;;   '(progn
-;;      (color-theme-initialize)
-;;      (color-theme-solarized-dark)
-;;      ))
+(require 'color-theme-solarized)
+(eval-after-load "color-theme"
+  '(progn
+     (color-theme-initialize)
+     (color-theme-solarized-dark)
+     ))
