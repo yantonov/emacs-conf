60f292c71986cd2dc4f766c11ee2a7d945bd7cca
Yury Antonov <yantonov@yandex.ru>
2013-02-12 01:51:13 +0600

    Popup attached to yasnippet.

--- rc/emacs-rc-yasnippet.el
+++ rc/emacs-rc-yasnippet.el
@@ -1,6 +1,30 @@
 (require 'yasnippet)
-(add-to-list 'yas-snippet-dirs (concat emacs-home "/snippets"))
+(add-to-list 'yas-snippet-dirs (concat emacs-home "snippets"))
 (yas-global-mode 1)
 
+;;; use popup menu for yas-choose-value
+(require 'popup)
 
+;; add some shotcuts in popup menu mode
+(define-key popup-menu-keymap (kbd "M-n") 'popup-next)
+(define-key popup-menu-keymap (kbd "TAB") 'popup-next)
+(define-key popup-menu-keymap (kbd "<tab>") 'popup-next)
+(define-key popup-menu-keymap (kbd "<backtab>") 'popup-previous)
+(define-key popup-menu-keymap (kbd "M-p") 'popup-previous)
 
+(defun yas-popup-isearch-prompt (prompt choices &optional display-fn)
+  (when (featurep 'popup)
+    (popup-menu*
+     (mapcar
+      (lambda (choice)
+        (popup-make-item
+         (or (and display-fn (funcall display-fn choice))
+             choice)
+         :value choice))
+      choices)
+     :prompt prompt
+     ;; start isearch mode immediately
+     :isearch t
+     )))
+
+(setq yas-prompt-functions '(yas-popup-isearch-prompt yas-ido-prompt yas-no-prompt))
