bc9038b7292996aa63ae82eb985935b3735d2bc3
Yury Antonov <yantonov@yandex.ru>
2013-02-16 11:56:20 +0600

    Fixed erlang-mode config.

--- init.el
+++ init.el
@@ -1,44 +1,78 @@
+;;
 ;; paths, environment
+;;
 (defvar user-home (if (getenv "HOME") (getenv "HOME") (expand-file-name "~"))
   "Defines user home directory.")
 (defvar emacs-home (concat user-home "/emacs/")
   "Defines emacs config directory inside user home directory.")
 (defvar emacs-mode-home (concat emacs-home "mode/")
-      "Defines directory for emacs extensions and modes.")
-;; for windows move explicitly to user home directory
-(cd user-home)
+  "Defines directory for emacs extensions and modes.")
 
 (defvar custom-init-before-file (concat user-emacs-directory "custom-before.el")
-      "Defines custom initialization file.
+  "Defines custom initialization file.
 This file is loaded before any settings for mode is loaded.
 So you can define environment paths of other global settings it this file.")
 (defvar custom-init-after-file (concat user-emacs-directory "custom-after.el")
-      "Defines custom initialization file.
+  "Defines custom initialization file.
 This file is loaded after any settings for mode is loaded.
 So you can override any previous defined settings using this file.")
 
+;;
+;; erlang environemnt variables
+;;
+(defvar erlang-mode-home nil
+  "Defines directory for erlang-mode. This variable is machine specific, can be defined in `custom-init-before-file' init file.
+
+Examples:
+
+Ubuntu (assumed erlang is installed from deb package provided by https://www.erlang-solutions.com) :
+
+\(setq erlang-mode-home \"/usr/lib/erlang/lib/tools-<ToolsVer>/emacs\"\)
+
+Windows
+\(setq erlang-mode-home \"C:/Program Files/erl<Ver>/lib/tools-<ToolsVer>/emacs\"\)")
+
+(defvar erlang-home nil
+  "Defines directory for erlang installation. This variable is machine specific, can be defined in `custom-init-before-file' init file.
+
+Examples:
+
+Ubuntu (assumed erlang is installed from deb package provided by https://www.erlang-solutions.com) :
+\(setq erlang-mode-home \"/usr/lib/erlang\"\)
+
+Windows
+\(setq erlang-mode-home \"C:/Program Files/erl<Ver>\"\)")
+
 (load custom-init-before-file 'noerror)
 
-;; modes
+;;
+;; modes paths
+;;
 (dolist
     (project (directory-files emacs-mode-home t "\\w+"))
   (when (file-directory-p project)
     (add-to-list 'load-path project)))
 
+;;
 ;; color themes
+;;
 (setq color-themes-dir (concat emacs-home "color-themes"))
 (dolist
     (project (directory-files color-themes-dir t "\\w+"))
   (when (file-directory-p project)
     (add-to-list 'load-path project)))
 
+;;
 ;; etc (core defuns and settings)
+;;
 (setq etc-dir (expand-file-name "etc" emacs-home))
 (dolist (file (directory-files etc-dir t "\\w+"))
   (when (file-regular-p file)
     (load file)))
 
+;;
 ;; defuns
+;;
 (setq defuns-dir (expand-file-name "defuns" emacs-home))
 (dolist (file (directory-files defuns-dir t "\\w+"))
   (when (file-regular-p file)
@@ -49,6 +83,10 @@ So you can override any previous defined settings using this file.")
   (when (file-regular-p file)
     (load file)))
 
+;; global hotkeys
 (load (concat emacs-home "hotkeys.el"))
 
 (load custom-init-after-file 'noerror)
+
+;; for windows move explicitly to user home directory
+(cd user-home)
--- rc/emacs-rc-erlang.el
+++ rc/emacs-rc-erlang.el
@@ -1,12 +1,34 @@
+;; erlang-mode-home, erlang-home is defined in ~/emacs/init.el
+
+(defun erlang-mode-exists ()
+  (and
+   (file-directory-p erlang-mode-home)
+   (file-exists-p (concat erlang-mode-home "/erlang.el")))
+  )
+
+(defun erlang-repl-exists()
+  (and (file-directory-p erlang-home)
+       (or (file-exists-p (concat erlang-home "/bin/erl"))
+           (file-exists-p (concat erlang-home "/bin/erl.exe"))))
+  )
+
+(defun erlang-envorinment-defined ()
+  (interactive)
+  (and (erlang-mode-exists) (erlang-repl-exists))
+  )
+
 (defun init-erlang-settings ()
   (interactive)
-  (add-to-list 'load-path "/usr/bin/erl")
+  (add-to-list 'load-path erlang-mode-home)
+
+  (setq erlang-root-dir erlang-home)
+  (add-to-list 'exec-path (concat erlang-root-dir "/bin"))
+  (setq erlang-man-root-dir (concat erlang-root-dir "/man"))
   (require 'erlang-start)
 
   (add-to-list 'auto-mode-alist '("\\.erl?$" . erlang-mode))
   (add-to-list 'auto-mode-alist '("\\.hrl?$" . erlang-mode))
-
-  (setq erlang-root-dir "/usr/bin/erl")
-  (add-to-list 'exec-path "/usr/bin/erl/bin")
-  (setq erlang-man-root-dir "/usr/bin/erl/man")
   )
+
+(if (erlang-envorinment-defined)
+    (init-erlang-settings))
