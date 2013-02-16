eca41cf7559c0b0fb89b807d4cfe942f8f3f3a78
Yury Antonov <yantonov@yandex.ru>
2013-02-16 13:17:07 +0600

    Undo-tree is installed using package manager not git repo.

--- install/install-undo-tree.sh
+++ install/install-undo-tree.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# http://www.dr-qubit.org/git/undo-tree.git
-
 SCRIPT_DIR=`dirname $0`
-URL=http://www.dr-qubit.org/git/undo-tree.git
-MODE_DIR="undo-tree"
-$SCRIPT_DIR/util/mode-from-git.sh ${URL} ${MODE_DIR}
-
+emacs -q -load $SCRIPT_DIR/util/install-undo-tree.el -kill
---
+++ install/util/install-undo-tree.el
@@ -0,0 +1,7 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(when (not (package-installed-p 'undo-tree))
+  (package-install 'undo-tree))
