dc6c32c365c16d9e2a7f1229ca829e686a2cfde7
Yury Antonov <yantonov@yandex.ru>
2013-03-19 02:36:04 +0600

    Not commited changes.

--- hooks.el
+++ hooks.el
@@ -1,9 +1,4 @@
 ;; org-mode
-(defun my-org-mode-hook ()
-  "My hook for org mode"
-  (interactive)
-  (my-general-keybindings org-mode-map)
-  )
 (add-hook 'org-mode-hook 'my-org-mode-hook)
 
 ;; paredit
