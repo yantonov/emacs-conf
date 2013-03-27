3f5f28051bdb565974ea48cf51f448c71f1020a4
Yury Antonov <yantonov@yandex.ru>
2013-03-28 00:17:41 +0600

    update utf-8 settings

--- rc/emacs-rc-editor.el
+++ rc/emacs-rc-editor.el
@@ -19,6 +19,7 @@
 (setq query-replace-highlight    t) ; Highlight query object
 
 ;; Always ALWAYS use UTF-8
+(set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
