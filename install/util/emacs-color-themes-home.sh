daa3f845b97a402fadf705be5bb4e984223554d8
Yury Antonov <yantonov@yandex.ru>
2013-04-05 00:33:21 +0600

    removed unused file

--- install/util/emacs-color-themes-home.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# prints emacs color themes home directory
-CURRENT_DIR=`dirname $0`
-EMACS_HOME=`${CURRENT_DIR}/emacs-home.sh`
-EMACS_MODE_HOME="$EMACS_HOME/color-themes"
-echo "${EMACS_MODE_HOME}"
