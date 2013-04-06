f6aa507e7415f0e60ba8f67e40fc73f581cb817e
Yury Antonov <yantonov@yandex.ru>
2013-04-06 10:41:57 +0600

    show-prog-keywords snippet was added for prog-mode, thanks @alexott for this usefull tool

---
+++ rc/emacs-rc-prog-mode.el
@@ -0,0 +1,18 @@
+;;; emacs-rc-prog-mode.el ---
+
+;; show FIXME/TODO/BUG keywords
+(defun my-show-prog-keywords ()
+  ;; highlight additional keywords
+  (font-lock-add-keywords nil '(("\\<\\(FIXME\\|TODO\\|BUG\\):" 1 font-lock-warning-face t)))
+  (font-lock-add-keywords nil '(("\\<\\(DONE\\):" 1 font-lock-doc-face t)))
+  ;; highlight too long lines
+  ;;(font-lock-add-keywords nil '(("^[^\n]\\{120\\}\\(.*\\)$" 1 font-lock-warning-face t)))
+  )
+
+(defun my-common-prog-hook ()
+  (subword-mode 1)
+  (my-show-prog-keywords))
+
+(add-hook 'prog-mode-hook 'my-common-prog-hook)
+
+;;; emacs-rc-prog-mode.el ends here---
