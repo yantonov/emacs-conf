1fdc1b4e139920a4332015840d224d1d7a48a7ec
Yury Antonov <yantonov@yandex.ru>
2013-04-19 00:15:17 +0600

    move hook for paredit in corresponding hook file (all hooks in one place)

--- hooks.el
+++ hooks.el
@@ -12,3 +12,6 @@
 (add-hook 'after-make-frame-functions 'reset-ui)
 ;; hook for setting up UI when not running in daemon mode
 (add-hook 'emacs-startup-hook 'reset-ui)
+
+;; enable paredit in minibuffer
+(add-hook 'minibuffer-setup-hook 'conditionally-enable-paredit-mode)
--- rc/emacs-rc-paredit.el
+++ rc/emacs-rc-paredit.el
@@ -48,5 +48,3 @@
   "Enable `paredit-mode' in the minibuffer, during `eval-expression'."
   (if (eq this-command 'eval-expression)
       (paredit-mode 1)))
-
-(add-hook 'minibuffer-setup-hook 'conditionally-enable-paredit-mode)
