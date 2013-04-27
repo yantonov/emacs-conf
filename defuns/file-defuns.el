932224c5ef6a764d985854223b3cffb70e8a5589
Yury Antonov <yantonov@yandex.ru>
2013-04-28 01:59:45 +0600

    sudo-edit snippet was added, thanks @bbatsov

--- defuns/file-defuns.el
+++ defuns/file-defuns.el
@@ -69,3 +69,15 @@
   (let ((file-buffer (or (buffer-file-name) ""))
         (command (read-shell-command "Shell command: " nil nil nil)))
     (shell-command (replace-regexp-in-string "%" file-buffer command))))
+
+(defun sudo-edit (&optional arg)
+  "Edit currently visited file as root.
+
+With a prefix ARG prompt for a file to visit.
+Will also prompt for a file to visit if current
+buffer is not visiting a file."
+  (interactive "P")
+  (if (or arg (not buffer-file-name))
+      (find-file (concat "/sudo:root@localhost:"
+                         (ido-read-file-name "Find file(as root): ")))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
