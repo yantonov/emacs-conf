1ad44f3191f22a254d28c05a8e05a58a7df28c2f
Yury Antonov <yantonov@yandex.ru>
2013-05-11 07:59:27 +0600

    backup and autosave files are now at tmp directory not near the original file

--- rc/emacs-rc-backup.el
+++ rc/emacs-rc-backup.el
@@ -1,7 +1,8 @@
-;; Write backup files to own directory
+;; store all backup and autosave files in the tmp dir
 (setq backup-directory-alist
-      `(("." . ,(expand-file-name
-                 (concat user-emacs-directory "backups")))))
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
 
 ;; Make backups of files, even when they're in version control
 (setq vc-make-backup-files t)
