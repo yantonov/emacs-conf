4ce6fc6053a04f87db42e5412b8529208f882254
Yury Antonov <yantonov@yandex.ru>
2013-05-21 10:06:16 +0600

    reformat

--- rc/emacs-rc-erlang.el
+++ rc/emacs-rc-erlang.el
@@ -7,33 +7,28 @@
   (and
    erlang-mode-home
    (file-directory-p erlang-mode-home)
-   (file-exists-p (concat erlang-mode-home "/erlang.el")))
-  )
+   (file-exists-p (concat erlang-mode-home "/erlang.el"))))
 
 (defun erlang-repl-exists()
   (and erlang-home
        (file-directory-p erlang-home)
        (or (file-exists-p (concat erlang-home "/bin/erl"))
-           (file-exists-p (concat erlang-home "/bin/erl.exe"))))
-  )
+           (file-exists-p (concat erlang-home "/bin/erl.exe")))))
 
 (defun erlang-envorinment-defined ()
   (interactive)
-  (and (erlang-mode-exists) (erlang-repl-exists))
-  )
+  (and (erlang-mode-exists) (erlang-repl-exists)))
 
 (defun init-erlang-settings ()
   (interactive)
   (add-to-list 'load-path erlang-mode-home)
-
   (setq erlang-root-dir erlang-home)
   (add-to-list 'exec-path (concat erlang-root-dir "/bin"))
   (setq erlang-man-root-dir (concat erlang-root-dir "/man"))
   (require 'erlang-start)
 
   (add-to-list 'auto-mode-alist '("\\.erl?$" . erlang-mode))
-  (add-to-list 'auto-mode-alist '("\\.hrl?$" . erlang-mode))
-  )
+  (add-to-list 'auto-mode-alist '("\\.hrl?$" . erlang-mode)))
 
 (if (erlang-envorinment-defined)
     (init-erlang-settings))
@@ -41,7 +36,6 @@
 (defun my-erlang-mode-hook ()
   (paredit-mode +1)
   (highlight-parentheses-mode t)
-  (rainbow-delimiters-mode-enable)
-  )
+  (rainbow-delimiters-mode-enable))
 
 (add-hook 'erlang-mode-hook 'my-erlang-mode-hook)
