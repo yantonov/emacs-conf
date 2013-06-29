04141cbde8845d278678c10b760697a89bd3848f
Yury Antonov <yantonov@yantonov-laptop.(none)>
2013-06-30 01:38:02 +0600

    remove byte compiled elisp file on save

--- rc/emacs-rc-elisp-mode.el
+++ rc/emacs-rc-elisp-mode.el
@@ -2,6 +2,15 @@
 (require 'highlight-parentheses)
 (require 'rainbow-delimiters)
 
+(defun remove-elc-on-save ()
+  "If you're saving an elisp file, likely the .elc is no longer valid."
+  (add-hook 'after-save-hook
+            (lambda ()
+              (if (file-exists-p (concat buffer-file-name "c"))
+                  (delete-file (concat buffer-file-name "c"))))
+            nil
+            t))
+
 (defun my-elisp-mode-hook ()
   (paredit-mode +1)
   (highlight-parentheses-mode t)
@@ -9,7 +18,8 @@
   ;; show-paren-mode
   ;; http://emacswiki.org/emacs/ShowParenMode
   (show-paren-mode 1)
-  (setq show-paren-style 'expression))
+  (setq show-paren-style 'expression)
+  (remove-elc-on-save))
 
 (add-hook 'emacs-lisp-mode-hook 'my-elisp-mode-hook)
 
