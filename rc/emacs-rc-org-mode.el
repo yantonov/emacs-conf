605ce4aa3b696c27748a95a984dff42fb503b116
Yury Antonov <yantonov@yandex.ru>
2013-08-29 01:10:44 +0600

    added settings for org-capture (org-mode)

--- rc/emacs-rc-org-mode.el
+++ rc/emacs-rc-org-mode.el
@@ -3,6 +3,8 @@
   (interactive)
   (my-general-keybindings org-mode-map))
 
+(setq org-default-notes-file "~/emacs/.todo-list.org")
+
 (add-hook 'org-mode-hook 'my-org-mode-hook)
 
 (provide 'emacs-rc-org-mode)
