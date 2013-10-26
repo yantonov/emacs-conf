a9fe703287ecdbb006ade4cef5f91384ed6bad5a
Yury Antonov <yantonov@yandex.ru>
2013-10-26 12:42:42 +0600

    mouse scroll settings

--- rc/emacs-rc-scroll.el
+++ rc/emacs-rc-scroll.el
@@ -7,4 +7,9 @@
       scroll-conservatively 10000
       scroll-preserve-screen-position 1)
 
+;; This snippet makes mouse wheel and trackpad scrolling bearable.
+;; Scroll in 1-line increments the buffer under the mouse.
+(setq mouse-wheel-follow-mouse 't)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+
 (provide 'emacs-rc-scroll)
