5231f3179f4a34cb5a989ad4eb0837ebbbc3c9c0
Yury Antonov <yantonov@yandex.ru>
2014-03-24 02:10:35 +0600

    lazy load for clojure test mode

--- rc/emacs-rc-clojure-test.el
+++ rc/emacs-rc-clojure-test.el
@@ -1,3 +1,4 @@
-(require 'clojure-test-mode)
+(autoload 'clojure-test-mode "clojure-test-mode.el" "clojure-test-mode" t)
+(add-to-list 'auto-mode-alist '("\\.clj$" . clojure-test-mode))
 
 (provide 'emacs-rc-clojure-test)
