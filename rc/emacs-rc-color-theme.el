8d1fde22af820e2d9cf98d28ccb796d6d8923598
Yury Antonov <yantonov@yandex.ru>
2014-04-16 11:19:50 +0600

    another defun prefix fix for color modes

--- rc/emacs-rc-color-theme.el
+++ rc/emacs-rc-color-theme.el
@@ -10,10 +10,10 @@
 ;; moe-theme
 ;; (load-theme 'moe-dark t)
 
-(add-hook 'after-make-window-system-frame-hooks
+(add-hook 'yantonov/after-make-window-system-frame-hooks
           '(lambda ()
              (load-theme 'solarized-dark t)))
-(add-hook 'after-make-console-frame-hooks
+(add-hook 'yantonov/after-make-console-frame-hooks
           '(lambda ()
              (load-theme 'zenburn t)))
 
