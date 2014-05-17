77ef666a40f033b97d485a4c5428872e537ae73f
Yury Antonov <yantonov@yandex.ru>
2014-05-17 15:29:10 +0600

    company-mode instead of auto-complete

--- install/install-autocomplete.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=`dirname $0`
-URL=https://github.com/auto-complete/auto-complete
-MODE_DIR="auto-complete"
-$SCRIPT_DIR/util/mode-from-git.sh ${URL} ${MODE_DIR}
--- rc/emacs-rc-auto-complete.el
+++
@@ -1,7 +0,0 @@
-(require 'auto-complete)
-(add-to-list 'ac-dictionary-directories (concat yantonov/emacs-mode-home "/ac-dict"))
-(require 'auto-complete-config)
-(ac-config-default)
-(setq ac-delay 0.5)
-
-(provide 'emacs-rc-auto-complete)
