bc03bc1b3e5d1661433289ab820f269b5c55db98
Yury Antonov <yantonov@yandex.ru>
2014-06-10 16:09:33 +0600

    disable global god mode by default

--- rc/emacs-rc-god-mode.el
+++ rc/emacs-rc-god-mode.el
@@ -1,11 +1,13 @@
 (require 'god-mode)
 
+(setq god-global-mode nil)
+
 (defun yantonov/my-update-cursor ()
   (setq cursor-type (if (or god-local-mode buffer-read-only)
                         'box
                       'bar)))
 
-(defun yantonov//god-mode-update-cursor ()
+(defun yantonov/god-mode-update-cursor ()
   (let ((limited-colors-p (> 257 (length (defined-colors)))))
     (cond (god-local-mode (progn
                             (set-face-background 'mode-line (if limited-colors-p "white" "#e9e2cb"))
@@ -16,7 +18,7 @@
 
 (defun yantonov/god-mode-toggle-hook ()
   (yantonov/my-update-cursor)
-  (yantonov//god-mode-update-cursor))
+  (yantonov/god-mode-update-cursor))
 
 (defun yantonov/god-mode-enabled-hook ()
   (yantonov/god-mode-toggle-hook))
