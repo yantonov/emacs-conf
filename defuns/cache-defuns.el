2244176a3948fc0a18e2612a6f66bae32cd906f7
Yury Antonov <yantonov@yandex.ru>
2014-11-08 07:50:07 +0500

    in case of hg or git executable is not found, pcomplete configuration failed at startup

---
+++ defuns/cache-defuns.el
@@ -0,0 +1,12 @@
+(defun cached (f)
+  "evaluate once and cache it"
+  (lexical-let ((value nil)
+                (fn f))
+    (lambda (&rest args)
+      (if (eq value nil)
+          (progn
+            (setq value (funcall fn))
+            value)
+        value))))
+
+(provide 'cache-defuns)
--- rc/emacs-rc-pcomplete.el
+++ rc/emacs-rc-pcomplete.el
@@ -1,22 +1,23 @@
 ;;**** Git Completion
-
 (defun pcmpl-git-commands ()
   "Return the most common git commands by parsing the git output."
+  (interactive)
   (with-temp-buffer
     (call-process-shell-command "git" nil (current-buffer) nil "help" "--all")
     (goto-char 0)
-    (search-forward "available git commands in")
-    (let (commands)
-      (while (re-search-forward
-              "^[[:blank:]]+\\([[:word:]-.]+\\)[[:blank:]]*\\([[:word:]-.]+\\)?"
-              nil t)
-        (push (match-string 1) commands)
-        (when (match-string 2)
-          (push (match-string 2) commands)))
-      (sort commands #'string<))))
+    (if (eq (search-forward "available git commands in") nil)
+        '("cant find git executable, check PATH varialbe")
+      (let (commands)
+        (while (re-search-forward
+                "^[[:blank:]]+\\([[:word:]-.]+\\)[[:blank:]]*\\([[:word:]-.]+\\)?"
+                nil t)
+          (push (match-string 1) commands)
+          (when (match-string 2)
+            (push (match-string 2) commands)))
+        (sort commands #'string<)))))
 
-(defconst pcmpl-git-commands (pcmpl-git-commands)
-  "List of `git' commands.")
+(defconst cached__pcmpl-git-commands (cached 'pcmpl-git-commands)
+  "Cached list of `git' commands.")
 
 (defvar pcmpl-git-ref-list-cmd "git for-each-ref refs/ --format='%(refname)'"
   "The `git' command to run to get a list of refs.")
@@ -38,10 +39,10 @@
 (defun pcomplete/git ()
   "Completion for `git'."
   ;; Completion for the command argument.
-  (pcomplete-here* pcmpl-git-commands)
+  (pcomplete-here* (funcall cached__pcmpl-git-commands))
   (cond
    ((pcomplete-match "help" 1)
-    (pcomplete-here* pcmpl-git-commands))
+    (pcomplete-here* (funcall cached__pcmpl-git-commands)))
    ((pcomplete-match (regexp-opt '("pull" "push")) 1)
     (pcomplete-here (pcmpl-git-remotes)))
    ;; provide branch completion for the command `checkout'.
@@ -55,34 +56,35 @@
 
 (defun pcmpl-hg-commands ()
   "Return the most common hg commands by parsing the hg output."
+  (interactive)
   (with-temp-buffer
     (call-process-shell-command "hg" nil (current-buffer) nil "-v" "help")
     (goto-char 0)
-    (search-forward "list of commands:")
-    (let (commands
-          (bound (save-excursion
-                   (re-search-forward "^[[:alpha:]]")
-                   (forward-line 0)
-                   (point))))
-      (while (re-search-forward
-              "^[[:blank:]]\\([[:word:]]+\\(?:, [[:word:]]+\\)*\\)" bound t)
-        (let ((match (match-string 1)))
-          (if (not (string-match "," match))
-              (push (match-string 1) commands)
-            (dolist (c (split-string match ", ?"))
-              (push c commands)))))
-      (sort commands #'string<))))
+    (if (eq (search-forward "list of comxmands:" nil t) nil)
+        '("cant find hg executable, check PATH variable")
+      (let (commands
+            (bound (save-excursion
+                     (re-search-forward "^[[:alpha:]]")
+                     (forward-line 0)
+                     (point))))
+        (while (re-search-forward
+                "^[[:blank:]]\\([[:word:]]+\\(?:, [[:word:]]+\\)*\\)" bound t)
+          (let ((match (match-string 1)))
+            (if (not (string-match "," match))
+                (push (match-string 1) commands)
+              (dolist (c (split-string match ", ?"))
+                (push c commands)))))
+        (sort commands #'string<)))))
 
-(defconst pcmpl-hg-commands (pcmpl-hg-commands)
-  "List of `hg' commands.")
+(defconst cached__pcmpl-hg-commands (cached 'pcmpl-hg-commands))
 
 (defun pcomplete/hg ()
   "Completion for `hg'."
   ;; Completion for the command argument.
-  (pcomplete-here* pcmpl-hg-commands)
+  (pcomplete-here* (funcall cached__pcmpl-hg-commands))
   (cond
    ((pcomplete-match "help" 1)
-    (pcomplete-here* pcmpl-hg-commands))
+    (pcomplete-here* (funcall cached__pcmpl-hg-commands)))
    (t
     (while (pcomplete-here (pcomplete-entries))))))
 
