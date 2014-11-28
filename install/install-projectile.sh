30f18b38003d020f891de7f4e4b893c02ebfa021
Yury Antonov <yantonov@yandex.ru>
2014-11-28 17:35:37 +0500

    install projectile using package manager, not from git

--- install/install-projectile.sh
+++ install/install-projectile.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# https://github.com/bbatsov/projectile.git
-
 SCRIPT_DIR=`dirname $0`
-URL=https://github.com/bbatsov/projectile.git
-MODE_DIR="projectile"
-$SCRIPT_DIR/util/mode-from-git.sh ${URL} ${MODE_DIR}
+${SCRIPT_DIR}/util/install-using-emacs-pkg.sh projectile
 
