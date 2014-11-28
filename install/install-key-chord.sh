1bee347eee7f5812a1b3125e7e595703972d4b73
Yury Antonov <yantonov@yandex.ru>
2014-11-28 18:00:05 +0500

    install script for key-chord

--- install/install-key-chord.sh
+++ install/install-key-chord.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 SCRIPT_DIR=`dirname $0`
-URL="http://www.emacswiki.org/emacs/download/key-chord.el"
-MODE_DIR="key-chord"
-$SCRIPT_DIR/util/single-file-mode.sh ${URL} ${MODE_DIR}
 
+${SCRIPT_DIR}/util/install-using-emacs-pkg.sh key-chord
