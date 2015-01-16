b22d48637e657aadde6513160babb1922545eca9
Yury Antonov <yantonov@yandex.ru>
2015-01-16 17:28:44 +0500

    marmalade repo moved to https

--- etc/etc-package.el
+++ etc/etc-package.el
@@ -1,6 +1,6 @@
 (require 'package)
 (setq package-archives '(("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
 
