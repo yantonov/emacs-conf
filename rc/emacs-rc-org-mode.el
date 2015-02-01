e83e335590013331f85a161fb2ee3b155d3ddfb4
Yury Antonov <yantonov@yandex.ru>
2015-02-01 15:21:23 +0500

    comment my org-mode-hook the purpose is to find org-mode keybinding problems

--- rc/emacs-rc-org-mode.el
+++ rc/emacs-rc-org-mode.el
@@ -1,10 +1,9 @@
-(defun yantonov/org-mode-hook ()
-  "My hook for org mode"
-  (interactive)
-  (yantonov/apply-general-keybindings org-mode-map))
+;; (defun yantonov/org-mode-hook ()
+;;   (interactive)
+;;   (yantonov/kbd-conf-general org-mode-map))
 
 (setq org-default-notes-file "~/emacs/.todo-list.org")
 
-(add-hook 'org-mode-hook 'yantonov/org-mode-hook)
+;; (add-hook 'org-mode-hook 'yantonov/org-mode-hook)
 
 (provide 'emacs-rc-org-mode)
