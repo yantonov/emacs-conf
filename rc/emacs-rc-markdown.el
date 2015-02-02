e2408893b57bef483b06a3cae1498ce7538cc07f
Yury Antonov <yantonov@yandex.ru>
2015-02-02 11:40:32 +0500

    enable markdown for *.markdown not only for *.md

--- rc/emacs-rc-markdown.el
+++ rc/emacs-rc-markdown.el
@@ -1,9 +1,8 @@
-;; markdown settings
 (autoload 'markdown-mode
   "markdown-mode.el"
   "Major mode for editing Markdown files" t)
-(setq auto-mode-alist
-      (cons '("\\.md" . markdown-mode)
-            auto-mode-alist))
+
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
 (provide 'emacs-rc-markdown)
