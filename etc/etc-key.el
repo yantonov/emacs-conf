1cab0381b06730ec5482586d2371e2422a6b3989
Yury Antonov <yantonov@yandex.ru>
2015-02-06 10:55:09 +0500

    restore missing keybindings

--- etc/etc-key.el
+++ etc/etc-key.el
@@ -177,7 +177,9 @@
         (setq mac-control-modifier 'control)
         (setq ns-function-modifier 'hyper)
         (define-key key-map (kbd "M-_") 'undo)
-        (define-key key-map (kbd "M-N") 'projectile-find-file))))
+        (define-key key-map (kbd "M-N") 'projectile-find-file)
+        (define-key key-map (kbd "H-SPC") 'set-mark-command)
+        (define-key key-map (kbd "C-M-m") 'yantonov/toggle-fullscreen))))
 
 (defun yantonov/kbd-conf-win (key-map)
   "see http://ergoemacs.org/emacs/emacs_hyper_super_keys.html for details"
