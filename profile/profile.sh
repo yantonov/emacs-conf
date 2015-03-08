e89ea13b797d86b2a3b136fd2bb69ebd92fa6fca
Yury Antonov <yantonov@yandex.ru>
2015-03-08 15:21:56 +0500

    script to profile emacs loading

---
+++ profile/profile.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(cd `dirname $0` && pwd)
+INIT_EL_PATH=$(cd $SCRIPT_DIR/../ && echo `pwd`/init.el)
+
+echo abspath $INIT_EL_PATH
+
+emacs -Q -l $SCRIPT_DIR/profile-dotemacs.el --eval "(setq profile-dotemacs-file  (setq load-file-name \"$INIT_EL_PATH\"))" -f profile-dotemacs
