cf60b43932bb0d0d82f4f84ae0c4054883f68482
Yury Antonov <yantonov@yandex.ru>
2015-05-03 17:45:50 +0500

    ivy-mode 0.4.0 binds tab key to ivy-partial-or-done

--- rc/emacs-rc-swiper.el
+++ rc/emacs-rc-swiper.el
@@ -4,7 +4,8 @@
 ;; (setq projectile-completion-system 'ivy)
 
 (defun yantonov/ivy-mode-hook()
-  (define-key ivy-minibuffer-map (kbd "TAB") 'ivy-alt-done))
+  ;;(define-key ivy-minibuffer-map (kbd "TAB") 'ivy-alt-done)
+  )
 
 (add-hook 'ivy-mode-hook 'yantonov/ivy-mode-hook)
 
