fa9f76c327f559fd307760d35ef4aad7835a9006
Yury Antonov <yantonov@yandex.ru>
2015-05-13 10:48:47 +0500

    install ace-window using emacs package system because of transitive dependencies

--- install/install-ace-window.sh
+++ install/install-ace-window.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 SCRIPT_DIR=`dirname $0`
-URL=https://github.com/abo-abo/ace-window.git
-MODE_DIR="ace-window"
-$SCRIPT_DIR/util/mode-from-git.sh ${URL} ${MODE_DIR}
 
+${SCRIPT_DIR}/util/install-using-emacs-pkg.sh ace-window
