4efa16f0884c9315f12d68bc127081e5b5d68ce7
Yury Antonov <yantonov@yandex.ru>
2015-07-08 10:47:31 +0500

    its possible to update only git repositories, only emacs packages or update all artifacts

--- install/update.sh
+++ install/update.sh
@@ -2,5 +2,25 @@
 
 SCRIPT_DIR=$(cd `dirname $0` && pwd)
 
-$SCRIPT_DIR/util/git-pull-all.sh
-$SCRIPT_DIR/util/update-installed-emacs-packages.sh
+target=$1
+
+case $target in
+    git)
+        $SCRIPT_DIR/util/git-pull-all.sh
+        ;;
+    emacs)
+        $SCRIPT_DIR/util/update-installed-emacs-packages.sh
+        ;;
+    all)
+        $SCRIPT_DIR/util/git-pull-all.sh
+        $SCRIPT_DIR/util/update-installed-emacs-packages.sh
+        ;;
+    *)
+        $SCRIPT_DIR/util/git-pull-all.sh
+        $SCRIPT_DIR/util/update-installed-emacs-packages.sh
+        ;;
+esac
+
+
+
+
