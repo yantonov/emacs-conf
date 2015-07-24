5c4a3a13f60282c3c68d3864115245a8d0d26eb4
Yury Antonov <yantonov@yandex.ru>
2015-07-25 01:45:49 +0500

    call go fmt on save to apply go-lang format specification

--- rc/emacs-rc-go-mode.el
+++ rc/emacs-rc-go-mode.el
@@ -1,3 +1,15 @@
 (require 'go-mode)
 
+(if (getenv "GOROOT")
+    (progn
+      (setq exec-path (cons (getenv "GOROOT") exec-path))))
+
+;; todo install only for go-mode
+(defun yantonov/my-go-mode-before-save-hook ()
+  ;;(message major-mode)
+  (when (eq major-mode 'go-mode)
+    (gofmt-before-save)))
+
+(add-hook 'before-save-hook 'yantonov/my-go-mode-before-save-hook)
+
 (provide 'emacs-rc-go-mode)
