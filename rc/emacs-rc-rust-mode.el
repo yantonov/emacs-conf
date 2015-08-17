0cce8f2496a6bf9c5fe087a48b76a1fb77eb7676
Yury Antonov <yantonov@yandex.ru>
2015-08-17 22:53:07 +0500

    do not crash in case of non existing racer

--- rc/emacs-rc-rust-mode.el
+++ rc/emacs-rc-rust-mode.el
@@ -1,18 +1,20 @@
 (autoload 'rust-mode "rust-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
 
-(add-hook 'rust-mode-hook
-          (lambda ()
-            (setq rust-indent-offset 4)
-            (setq )))
+(defun yantonov/rust-mode-hook ()
+  (setq rust-indent-offset 4))
 
-(setq racer-rust-src-path  (getenv "RUST_SRC_PATH"))
-(setq racer-cmd (getenv "RACER_HOME"))
-(add-to-list 'load-path (concat (getenv "RACER_HOME") "/editors/emacs/"))
-(eval-after-load "rust-mode"
-  '(progn
-     (require 'company)
-     (require 'racer)))
+(defun yantonov/racer-defined-p ()
+  (and (getenv "RACER_HOME")
+       (file-exists-p (getenv "RACER_HOME"))))
+
+(defun yantonov/racer-init ()
+  (if (yantonov/racer-defined-p)
+      (progn
+        (setq racer-rust-src-path  (getenv "RUST_SRC_PATH"))
+        (setq racer-cmd (getenv "RACER_HOME"))
+        (add-to-list 'load-path (concat (getenv "RACER_HOME")
+                                        "/editors/emacs/")))))
 
 (defun yantonov/rust-compile-hook ()
   (require 'compile)
@@ -23,7 +25,15 @@
          (format "rustc %s && %s" (buffer-file-name)
                  (file-name-sans-extension (buffer-file-name))))))
 
-;; (setq-default compilation-read-command nil)
+(add-hook 'rust-mode-hook 'yantonov/rust-mode-hook)
 (add-hook 'rust-mode-hook 'yantonov/rust-compile-hook)
+
+(yantonov/racer-init)
+
+(eval-after-load "rust-mode"
+  '(progn
+     (require 'company)
+     (if (yantonov/racer-defined-p)
+         (require 'racer))))
 
 (provide 'emacs-rc-rust-mode)
