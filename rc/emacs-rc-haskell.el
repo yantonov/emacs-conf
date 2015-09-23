a016d1c5b71c88f4e5550c92e8a1c07f5369ef89
Yury Antonov <yantonov@yandex.ru>
2015-09-23 13:00:25 +0500

    search at hoogle and hayoo

--- rc/emacs-rc-haskell.el
+++ rc/emacs-rc-haskell.el
@@ -9,8 +9,20 @@
 (add-to-list 'company-backends 'company-ghc)
 (custom-set-variables '(company-ghc-show-info t))
 
+(defun yantonov/search-hoogle ()
+  (interactive)
+  (yantonov/search-internal "http://www.haskell.org/hoogle/?hoogle=" "Hoogle: "))
+
+(defun yantonov/search-hayoo ()
+  (interactive)
+  (yantonov/search-internal "http://hayoo.fh-wedel.de/?query=" "Hayoo: "))
+
+
 (defun yantonov/haskell-mode-hook ()
   (local-unset-key (kbd "C-M-l"))
+  (local-unset-key (kbd "C-c -h"))
+  (local-set-key (kbd "C-c C-s C-h") 'yantonov/search-hoogle)
+  (local-set-key (kbd "C-c C-s C-y") 'yantonov/search-hayoo)
   (turn-on-haskell-doc-mode)
   (turn-on-haskell-indentation)
   (company-mode)
