5ce50a1af1571579440b694f02c782fe10c9aa5e
Yury Antonov <yantonov@yandex.ru>
2015-10-30 15:12:30 +0500

    melpa

--- etc/etc-package.el
+++ etc/etc-package.el
@@ -1,7 +1,8 @@
 (require 'package)
 (setq package-archives '(("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")
                          ("marmalade" . "https://marmalade-repo.org/packages/")
-                         ("gnu" . "http://elpa.gnu.org/packages/")))
+                         ("gnu" . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")))
 (package-initialize)
 
 (provide 'etc-package)
