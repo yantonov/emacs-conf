d92b798417dfdb08275a11d56dc25cd314c27ca2
Yury Antonov <yantonov@yandex.ru>
2016-01-07 23:29:03 +0500

    service scripts are moved into bin dir

--- bin/tools/profile/profile.sh
+++ bin/tools/profile/profile.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 SCRIPT_DIR=$(cd `dirname $0` && pwd)
-INIT_EL_PATH=$(cd $SCRIPT_DIR/../ && echo `pwd`/init.el)
-
-echo abspath $INIT_EL_PATH
+INIT_EL_PATH=$(cd $SCRIPT_DIR/../../ && echo `pwd`/init.el)
 
 emacs -Q -l $SCRIPT_DIR/profile-dotemacs.el --eval "(setq profile-dotemacs-file  (setq load-file-name \"$INIT_EL_PATH\"))" -f profile-dotemacs
