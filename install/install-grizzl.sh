04a0efba64876bf13699746ed065ee07722c11d7
Yury Antonov <yantonov@yandex.ru>
2016-01-07 23:05:13 +0500

    oops switch to grizzl back

---
+++ install/install-grizzl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+SCRIPT_DIR=`dirname $0`
+URL="https://github.com/d11wtq/grizzl.git"
+MODE_DIR="grizzl"
+$SCRIPT_DIR/util/mode-from-git.sh ${URL} ${MODE_DIR}
--- rc/emacs-rc-projectile.el
+++ rc/emacs-rc-projectile.el
@@ -1,7 +1,11 @@
 (require 'projectile)
+(require 'grizzl)
 ;; enable globally
 ;; (projectile-global-mode)
 
 ;; to enable for some mode
 ;; (add-hook 'clojure-mode-hook 'projectile-on)
+
+(setq projectile-completion-system 'grizzl)
+
 (provide 'emacs-rc-projectile)
