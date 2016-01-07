fd05ee534284a8355113f8051fa3e40cf994f447
Yury Antonov <yantonov@yandex.ru>
2016-01-08 00:22:52 +0500

    kill trash

--- etc/etc-daemon-init.el
+++
@@ -1,8 +0,0 @@
-(add-hook 'after-make-window-system-frame-hooks
-          '(lambda ()
-             (load-theme 'solarized-dark t)))
-(add-hook 'after-make-console-frame-hooks
-          '(lambda ()
-             (load-theme 'zenburn t)))
-
-(provide 'etc-daemon-init)
