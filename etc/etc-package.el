376a441ed93b805cb48a633134b07a9ebe8248e3
Yury Antonov <yantonov@yandex.ru>
2016-01-09 16:05:58 +0500

    use cider from melpa-stable

--- etc/etc-package.el
+++ etc/etc-package.el
@@ -2,6 +2,9 @@
 (setq package-archives '(("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")
                          ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("gnu" . "http://elpa.gnu.org/packages/")))
+
+(add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
+
 (package-initialize)
 
 (require 'use-package)
