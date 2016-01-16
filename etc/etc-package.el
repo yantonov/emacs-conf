9369671ae7ca6eced0bb97ec4701b1cd887e5252
Yury Antonov <yantonov@yandex.ru>
2016-01-16 10:34:59 +0500

    use some packages from melpa-stable

--- etc/etc-package.el
+++ etc/etc-package.el
@@ -3,8 +3,8 @@
                          ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("gnu" . "http://elpa.gnu.org/packages/")))
 
-(add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
-(add-to-list 'package-pinned-packages '(clj-refactor . "melpa-stable") t)
+;; (add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
+;; (add-to-list 'package-pinned-packages '(clj-refactor . "melpa-stable") t)
 
 (package-initialize)
 
