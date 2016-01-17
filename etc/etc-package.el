0cfd79fe2d267de04839f28ddcba41de37c073ce
Yury Antonov <yantonov@yandex.ru>
2016-01-17 15:49:13 +0500

    use clj-refactor from melpa

--- etc/etc-package.el
+++ etc/etc-package.el
@@ -1,10 +1,11 @@
 (require 'package)
 (setq package-archives '(("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")
+                         ("melpa" . "https://melpa.org/packages/")
                          ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("gnu" . "http://elpa.gnu.org/packages/")))
 
-;; (add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
-;; (add-to-list 'package-pinned-packages '(clj-refactor . "melpa-stable") t)
+(add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
+(add-to-list 'package-pinned-packages '(clj-refactor . "melpa") t)
 
 (package-initialize)
 
