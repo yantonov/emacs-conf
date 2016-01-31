2f4f1ca0c62a1b54afe266999e76c5224e85ebe6
Yury Antonov <yantonov@yandex.ru>
2016-01-31 21:58:27 +0500

    flycheck, flycheck-rust

---
+++ rc/emacs-rc-flycheck.el
@@ -0,0 +1,3 @@
+(require 'flycheck)
+
+(provide 'emacs-rc-flycheck)
--- rc/emacs-rc-rust-mode.el
+++ rc/emacs-rc-rust-mode.el
@@ -1,55 +1,64 @@
-(autoload 'rust-mode "rust-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+(use-package rust-mode
+  :defer t
+  :init
+  (progn
+    (autoload 'rust-mode "rust-mode" nil t)
+    (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode)))
+  :config
+  (progn
 
-(defun yantonov/racer-defined-p ()
-  (and (getenv "RACER_HOME")
-       (file-exists-p (getenv "RACER_HOME"))))
+    (defun yantonov/racer-defined-p ()
+      (and (getenv "RACER_HOME")
+           (file-exists-p (getenv "RACER_HOME"))))
 
-(defun yantonov/search-rust-doc ()
-  "Search https://doc.rust-lang.org/std/"
-  (interactive)
-  (yantonov/search-internal "https://doc.rust-lang.org/std/?search=" "Search doc.rust-lang.org/std: "))
+    (defun yantonov/search-rust-doc ()
+      "Search https://doc.rust-lang.org/std/"
+      (interactive)
+      (yantonov/search-internal "https://doc.rust-lang.org/std/?search=" "Search doc.rust-lang.org/std: "))
 
-(defun yantonov/rust-mode-hook ()
-  (setq rust-indent-offset 4)
-  (company-mode)
-  (setq company-idle-delay 0.2)
-  (setq company-minimum-prefix-length 1)
-  (if (yantonov/racer-defined-p)
-      (progn
-        (racer-activate)
-        (eldoc-mode)
-        (local-set-key (kbd "M-.") #'racer-find-definition)
-        (local-set-key (kbd "TAB") #'racer-complete-or-indent)
-        (local-set-key (kbd "C-c C-c") #'yantonov/toggle-camelcase-underscores)
-        (local-set-key (kbd "C-c C-d") #'yantonov/search-rust-doc))))
+    (defun yantonov/rust-mode-hook ()
+      (setq rust-indent-offset 4)
+      (company-mode)
+      (setq company-idle-delay 0.2)
+      (setq company-minimum-prefix-length 1)
+      (if (yantonov/racer-defined-p)
+          (progn
+            (racer-activate)
+            (eldoc-mode)
+            (local-set-key (kbd "M-.") #'racer-find-definition)
+            (local-set-key (kbd "TAB") #'racer-complete-or-indent)
+            (local-set-key (kbd "C-c C-c") #'yantonov/toggle-camelcase-underscores)
+            (local-set-key (kbd "C-c C-d") #'yantonov/search-rust-doc))))
 
-(defun yantonov/racer-init ()
-  (if (yantonov/racer-defined-p)
-      (progn
-        (setq racer-rust-src-path  (getenv "RUST_SRC_PATH"))
-        (setq racer-cmd (concat (getenv "RACER_HOME")
-                                "/target/release/racer"))
-        (add-to-list 'load-path (concat (getenv "RACER_HOME")
-                                        "/editors/emacs/"))
-        (require 'racer))))
+    (defun yantonov/racer-init ()
+      (if (yantonov/racer-defined-p)
+          (progn
+            (setq racer-rust-src-path  (getenv "RUST_SRC_PATH"))
+            (setq racer-cmd (concat (getenv "RACER_HOME")
+                                    "/target/release/racer"))
+            (add-to-list 'load-path (concat (getenv "RACER_HOME")
+                                            "/editors/emacs/"))
+            (require 'racer))))
 
-(defun yantonov/rust-compile-hook ()
-  (set (make-local-variable 'compile-command)
-       (if (locate-dominating-file (buffer-file-name) "Cargo.toml")
-           "cargo run"
-         (format "rustc %s && %s" (buffer-file-name)
-                 (file-name-sans-extension (buffer-file-name))))))
+    (defun yantonov/rust-compile-hook ()
+      (set (make-local-variable 'compile-command)
+           (if (locate-dominating-file (buffer-file-name) "Cargo.toml")
+               "cargo run"
+             (format "rustc %s && %s" (buffer-file-name)
+                     (file-name-sans-extension (buffer-file-name))))))
 
-(add-hook 'rust-mode-hook 'yantonov/rust-mode-hook)
-(add-hook 'rust-mode-hook 'yantonov/rust-compile-hook)
+    (add-hook 'rust-mode-hook 'yantonov/rust-mode-hook)
+    (add-hook 'rust-mode-hook 'yantonov/rust-compile-hook)
 
-(eval-after-load "rust-mode"
-  '(progn
-     (require 'company)
-     (require 'compile)
-     (require 'projectile)
-     (if (yantonov/racer-defined-p)
-         (yantonov/racer-init))))
+    (require 'emacs-rc-company-mode)
+    (require 'compile)
+    (require 'emacs-rc-projectile)
+    (require 'emacs-rc-flycheck)
+
+    (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+    (flycheck-mode)
+
+    (if (yantonov/racer-defined-p)
+        (yantonov/racer-init))))
 
 (provide 'emacs-rc-rust-mode)
