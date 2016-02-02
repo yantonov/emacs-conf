55cdafee34b68a11b0c2b90b732d297314a7829a
Yury Antonov <yantonov@yandex.ru>
2016-02-02 09:08:54 +0500

    change map for clj-refactor, because of C-c C-m is used by cider for macroexpansion

--- rc/emacs-rc-clojure-mode.el
+++ rc/emacs-rc-clojure-mode.el
@@ -29,7 +29,7 @@
       (clj-refactor-mode 1)
       (yas-minor-mode 1) ; for adding require/use/import statements
       ;; This choice of keybinding leaves cider-macroexpand-1 unbound
-      (cljr-add-keybindings-with-prefix "C-c C-m")
+      (cljr-add-keybindings-with-prefix "C-c m")
       ;; hightlight-parentheses
       (yantonov/init-highlight-parentheses)
       (highlight-parentheses-mode))
