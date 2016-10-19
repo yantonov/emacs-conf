14ee411cb071552b4cd348eae655bdaf992c03b3
Yury Antonov <yntv@yandex-team.ru>
2016-10-19 18:52:58 +0500

    temporary disable parinfer for clojure/elisp modes

--- rc/emacs-rc-parinfer-mode.el
+++ rc/emacs-rc-parinfer-mode.el
@@ -5,7 +5,8 @@
     (defun yantonov/enable-parinfer-mode ()
       (parinfer-mode)
       (parinfer-toggle-mode))
-    (add-hook 'clojure-mode-hook #'yantonov/enable-parinfer-mode)
-    (add-hook 'emacs-lisp-mode-hook #'yantonov/enable-parinfer-mode)))
+    ;; (add-hook 'clojure-mode-hook #'yantonov/enable-parinfer-mode)
+    ;; (add-hook 'emacs-lisp-mode-hook #'yantonov/enable-parinfer-mode)
+    ))
 
 (provide 'emacs-rc-parinfer-mode)
