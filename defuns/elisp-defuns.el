2a891627b0747129894157e211169c401e97a009
Yury Antonov <yantonov@yandex.ru>
2016-12-19 07:56:09 +0500

    comment region if exists

--- defuns/elisp-defuns.el
+++ defuns/elisp-defuns.el
@@ -2,51 +2,51 @@
   "Uncomment a sexp around point."
   (interactive "P")
   (let* ((initial-point (point-marker))
-     (p)
-     (end (save-excursion
-        (when (elt (syntax-ppss) 4)
-          (re-search-backward comment-start-skip
-                      (line-beginning-position)
-                      t))
-        (setq p (point-marker))
-        (comment-forward (point-max))
-        (point-marker)))
-     (beg (save-excursion
-        (forward-line 0)
-        (while (= end (save-excursion
+         (p)
+         (end (save-excursion
+                (when (elt (syntax-ppss) 4)
+                  (re-search-backward comment-start-skip
+                                      (line-beginning-position)
+                                      t))
+                (setq p (point-marker))
                 (comment-forward (point-max))
-                (point)))
-          (forward-line -1))
-        (goto-char (line-end-position))
-        (re-search-backward comment-start-skip
-                    (line-beginning-position)
-                    t)
-        (while (looking-at-p comment-start-skip)
-          (forward-char -1))
-        (point-marker))))
+                (point-marker)))
+         (beg (save-excursion
+                (forward-line 0)
+                (while (= end (save-excursion
+                                (comment-forward (point-max))
+                                (point)))
+                  (forward-line -1))
+                (goto-char (line-end-position))
+                (re-search-backward comment-start-skip
+                                    (line-beginning-position)
+                                    t)
+                (while (looking-at-p comment-start-skip)
+                  (forward-char -1))
+                (point-marker))))
     (unless (= beg end)
       (uncomment-region beg end)
       (goto-char p)
       ;; Indentify the "top-level" sexp inside the comment.
       (while (and (ignore-errors (backward-up-list) t)
-          (>= (point) beg))
-    (skip-chars-backward (rx (syntax expression-prefix)))
-    (setq p (point-marker)))
+                  (>= (point) beg))
+        (skip-chars-backward (rx (syntax expression-prefix)))
+        (setq p (point-marker)))
       ;; Re-comment everything before it.
       (ignore-errors
-    (comment-region beg p))
+        (comment-region beg p))
       ;; And everything after it.
       (goto-char p)
       (forward-sexp (or n 1))
       (skip-chars-forward "\r\n[:blank:]")
       (if (< (point) end)
-      (ignore-errors
-        (comment-region (point) end))
-    ;; If this is a closing delimiter, pull it up.
-    (goto-char end)
-    (skip-chars-forward "\r\n[:blank:]")
-    (when (= 5 (car (syntax-after (point))))
-      (delete-indentation))))
+          (ignore-errors
+            (comment-region (point) end))
+        ;; If this is a closing delimiter, pull it up.
+        (goto-char end)
+        (skip-chars-forward "\r\n[:blank:]")
+        (when (= 5 (car (syntax-after (point))))
+          (delete-indentation))))
     ;; Without a prefix, it's more useful to leave point where
     ;; it was.
     (unless n
@@ -55,9 +55,9 @@
 (defun comment-sexp--raw ()
   "Comment the sexp at point or ahead of point."
   (pcase (or (bounds-of-thing-at-point 'sexp)
-         (save-excursion
-           (skip-chars-forward "\r\n[:blank:]")
-           (bounds-of-thing-at-point 'sexp)))
+             (save-excursion
+               (skip-chars-forward "\r\n[:blank:]")
+               (bounds-of-thing-at-point 'sexp)))
     (`(,l . ,r)
      (goto-char r)
      (skip-chars-forward "\r\n[:blank:]")
@@ -69,15 +69,17 @@
 If already inside (or before) a comment, uncomment instead.
 With a prefix argument N, (un)comment that many sexps."
   (interactive "P")
-  (if (or (elt (syntax-ppss) 4)
-      (< (save-excursion
-           (skip-chars-forward "\r\n[:blank:]")
-           (point))
-         (save-excursion
-           (comment-forward 1)
-           (point))))
-      (uncomment-sexp n)
-    (dotimes (_ (or n 1))
-      (comment-sexp--raw))))
+  (if (use-region-p)
+      (comment-or-uncomment-region (region-beginning) (region-end))
+    (if (or (elt (syntax-ppss) 4)
+            (< (save-excursion
+                 (skip-chars-forward "\r\n[:blank:]")
+                 (point))
+               (save-excursion
+                 (comment-forward 1)
+                 (point))))
+        (uncomment-sexp n)
+      (dotimes (_ (or n 1))
+        (comment-sexp--raw)))))
 
 (provide 'elisp-defuns)
