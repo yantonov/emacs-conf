ef497b97204921b5d565665ed5e74d9abaf0bea4
Yury Antonov <yantonov@yandex.ru>
2017-01-05 12:17:35 +0500

    smex+ido instead of helm

---
+++ rc/emacs-rc-smex.el
@@ -0,0 +1,11 @@
+(use-package smex
+  :defer nil
+  :init
+  (progn
+    (global-set-key (kbd "M-x") 'smex)
+    (global-set-key (kbd "M-X") 'smex-major-mode-commands)
+    ;; This is your old M-x.
+    (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)))
+
+
+(provide 'emacs-rc-smex)
