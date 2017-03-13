4651344319394fb54d2b61881bf7c65ba1b9ed39
Yury Antonov <yantonov@yandex.ru>
2017-03-13 10:43:13 +0500

    comment or uncomment region in sh-script mode

---
+++ rc/emacs-rc-sh-script-mode.el
@@ -0,0 +1,11 @@
+(use-package sh-script
+  :defer t
+  :config
+  (progn
+    (add-hook 'sh-mode-hook
+              (lambda ()
+                (define-key sh-mode-map (kbd "C-/") 'yantonov/smart-comment)))))
+
+
+
+(provide 'emacs-rc-sh-script-mode)
