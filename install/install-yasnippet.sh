5d2795772407000522c1316ddb9e4a889965024b
Yury Antonov <yantonov@yandex.ru>
2017-03-13 10:24:19 +0500

    yasnippet is installed as transitive dependency of clj-refactor

--- install/install-yasnippet.sh
+++ install/install-yasnippet.sh
@@ -3,5 +3,5 @@
 SCRIPT_DIR=`dirname $0`
 URL="https://github.com/capitaomorte/yasnippet.git"
 MODE_DIR="yasnippet"
-$SCRIPT_DIR/util/mode-from-git.sh ${URL} ${MODE_DIR}
+# $SCRIPT_DIR/util/mode-from-git.sh ${URL} ${MODE_DIR}
 
