9f3156968812c5a17cc6ab0dec14efdeb837c002
Yury Antonov <yantonov@yandex.ru>
2018-09-02 17:14:55 +0500

    remove ess

--- rc/emacs-rc-ess.el
+++
@@ -1,7 +0,0 @@
-(load (concat yantonov/modes-home "/ess/ess-autoloads.el"))
-
-(autoload 'R-mode "ess-site" "ESS" t)
-(add-to-list 'auto-mode-alist '("\\.R\\'" . R-mode))
-(add-to-list 'auto-mode-alist '("\\.r\\'" . R-mode))
-
-(provide 'emacs-rc-ess)
