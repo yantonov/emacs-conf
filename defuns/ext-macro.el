72e5c1c269de7b717b1efb5bfaba4dc1aa51fc02
Yury Antonov <yantonov@yandex.ru>
2020-09-08 00:18:10 +0500

    simplify code

--- defuns/ext-macro.el
+++ defuns/ext-macro.el
@@ -4,7 +4,7 @@
   "Return a function that runs the executable EXEC."
   (let ((func-name
          (intern (concat "yantonov/run-executable-" program-name)))
-        (args (if (eq nil program-args) '() program-args)))
+        (args (or program-args '())))
     `(progn
        (defun ,func-name ()
          ,(format "Run the %s executable [args=%s]." program-name args)
@@ -17,7 +17,7 @@
   "Return a function that calls `browse-url' on URL."
   (let ((func-name
          (intern (concat "yantonov/browse-url-"
-                         (if fn-alias fn-alias url)))))
+                         (or fn-alias url)))))
     `(progn
        (defun ,func-name ()
          ,(format "Browse to the url %s." url)
