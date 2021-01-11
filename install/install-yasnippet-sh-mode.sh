620390b3602a0f2dfa01025cd3e81e83e7d7e816
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2021-01-11 22:15:03 +0100

    use git protocol to clone yasnippet sh-mode

--- install/install-yasnippet-sh-mode.sh
+++ install/install-yasnippet-sh-mode.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-URL="https://github.com/yantonov/yasnippet-sh-mode.git"
+URL="git@github.com:yantonov/yasnippet-sh-mode.git"
 MODE_DIR="sh-mode"
 $SCRIPT_DIR/util/snippets-from-git.sh ${URL} ${MODE_DIR}
