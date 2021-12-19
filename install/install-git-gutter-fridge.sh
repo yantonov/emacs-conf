eda88a5fea253e0d116cddeddda81c9aeaafde61
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2021-12-19 23:14:24 +0100

    remove unused git-gutter

--- install/install-git-gutter-fridge.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=`dirname $0`
-URL=https://github.com/syohex/emacs-git-gutter-fringe.git
-MODE_DIR="git-gutter-fridge"
-$SCRIPT_DIR/util/mode-from-git.sh ${URL} ${MODE_DIR}
-
