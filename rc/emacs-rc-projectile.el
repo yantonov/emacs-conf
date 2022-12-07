fdf509c2ab93e14476d91d8f7a1c1397f870b8a1
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2022-12-07 22:31:27 +0100

    grizzl is no longer used

--- rc/emacs-rc-projectile.el
+++ rc/emacs-rc-projectile.el
@@ -1,11 +1,3 @@
 (require 'projectile)
-(require 'grizzl)
-;; enable globally
-;; (projectile-global-mode)
-
-;; to enable for some mode
-;; (add-hook 'clojure-mode-hook 'projectile-on)
-
-(setq projectile-completion-system 'grizzl)
 
 (provide 'emacs-rc-projectile)
