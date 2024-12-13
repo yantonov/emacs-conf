fbaa0be8056de59491ea2f62ff061faec2055218
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2024-12-13 23:53:40 +0100

    remove scroll settings

--- rc/emacs-rc-scroll.el
+++ rc/emacs-rc-scroll.el
@@ -2,12 +2,6 @@
 (if (fboundp 'scroll-bar-mode)
     (set-scroll-bar-mode nil))
 
-(setq redisplay-dont-pause t
-      scroll-margin 1
-      scroll-step 1
-      scroll-conservatively 10000
-      scroll-preserve-screen-position 1)
-
 ;; This snippet makes mouse wheel and trackpad scrolling bearable.
 ;; Scroll in 1-line increments the buffer under the mouse.
 (setq mouse-wheel-follow-mouse 't)
