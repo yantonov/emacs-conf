2ff6305ab37063bc9fb80882a8e1fe92d017350a
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2024-12-29 11:55:09 +0100

    add paredit config

---
+++ rc/emacs-rc-paredit.el
@@ -0,0 +1,14 @@
+(use-package paredit
+  :ensure t
+  :commands paredit-mode
+  :defines paredit-mode-map
+  :bind (:map paredit-mode-map
+              ;; ("C-<left>" . nil)
+              ;; ("C-<right>" . nil)
+              ("C-M-," . paredit-forward-barf-sexp)
+              ("C-M-." . paredit-forward-slurp-sexp))
+  :init
+  (add-hook 'clojure-mode-hook 'paredit-mode)
+  (add-hook 'emacs-lisp-mode-hook 'paredit-mode))
+
+(provide 'emacs-rc-paredit)
